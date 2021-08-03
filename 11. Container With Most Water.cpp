f32207a792532f1c691c31ac9e30240f0799bb91
Gokul Ganeshan I <igokulganesh@gmail.com>
2021-08-03 07:53:39 +0530

    Update 11. Container With Most Water.cpp

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -1,3 +1,20 @@
+// Brutte Force Method
+int maxArea(vector<int>& height) 
+{
+    int res = 0 ; 
+    
+    for(int i = 0 ; i < height.size() ; i++)
+    {
+        for(int j = i+1 ; j < height.size() ; j++)
+        {
+            res = max(res, min(height[i], height[j]) * (j - i)); 
+        }
+    }
+    
+    return res ;
+}	
+
+// Optimal Apporach
 int maxArea(vector<int>& height) 
 {
 	int res = 0 ; 
