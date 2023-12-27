20de22bbe306c34375b3609b9e79a16fba13e72d
Gokul Ganesh I <igokulganesh@gmail.com>
2023-12-27 22:05:36 +0530

    Rust impl of 287. Find the Duplicate Number

---
+++ Rust/287. Find the Duplicate Number.rs
@@ -0,0 +1,21 @@
+pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+    let mut slow = nums[0];
+    let mut fast = nums[0];
+
+    loop {
+        slow = nums[slow as usize];
+        fast = nums[nums[fast as usize] as usize];
+
+        if slow == fast {
+            break;
+        }
+    }
+
+    slow = nums[0];
+    while slow != fast {
+        slow = nums[slow as usize];
+        fast = nums[fast as usize];
+    }
+
+    slow
+}
