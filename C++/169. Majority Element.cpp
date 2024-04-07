06835315ccbd6f950533fbe451ef9305c8722a58
Gokul Ganesh I <igokulganesh@gmail.com>
2024-04-07 23:10:10 +0530

    169. Majority Element for C++

--- C++/169. Majority Element.cpp
+++ C++/169. Majority Element.cpp
@@ -1,23 +1,27 @@
-int majorityElement(vector<int> & v)
-{        
-    unordered_map<int, int> m ; 
-    
-    for(int i = 0 ; i < v.size() ; i++)
-        m[v[i]]++ ; 
+#include <bits/stdc++.h>
 
-    int major = v[0] ; 
-    int count = m[v[0]] ; 
-    
-    for(auto i : m)
+using namespace std;
+
+int majorityElement(vector<int> &v)
+{
+    unordered_map<int, int> m;
+
+    for (int i = 0; i < v.size(); i++)
+        m[v[i]]++;
+
+    int major = v[0];
+    int count = m[v[0]];
+
+    for (auto i : m)
     {
-        if(count <= i.second)
+        if (count <= i.second)
         {
-            count = i.second ;
-            major = i.first ; 
+            count = i.second;
+            major = i.first;
         }
     }
-    
-    return major ; 
+
+    return major;
 }
 
 /*
@@ -28,33 +32,32 @@ sort and return the middle element
 Time: O(nLogn)
 space: O(1)
 
-*/ 
+*/
 
-int majorityElement(vector<int>& nums) 
+int majorityElement(vector<int> &nums)
 {
     sort(nums.begin(), nums.end());
-    return nums[nums.size()/2];
+    return nums[nums.size() / 2];
 }
 
-
 /*
-    Best Solution using moor's voting Alg 
+    Best Solution using moor's voting Alg
 */
-int majorityElement(vector<int>& nums) 
+int majorityElement(vector<int> &nums)
 {
-    int count = 1 ;
-    int major = nums[0] ; 
+    int count = 1;
+    int major = nums[0];
 
-    for(int i = 1 ; i < nums.size() ; i++)
+    for (int i = 1; i < nums.size(); i++)
     {
-        if(count == 0)
-            major = nums[i] ; 
-        
-        if(major == nums[i])
-            count++ ; 
+        if (count == 0)
+            major = nums[i];
+
+        if (major == nums[i])
+            count++;
         else
-            count-- ; 
+            count--;
     }
 
-    return major ; 
+    return major;
 }
