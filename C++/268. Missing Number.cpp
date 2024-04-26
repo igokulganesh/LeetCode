3c6a6339ec7a304625163406a3ca7e9082ee8e18
Gokul Ganesh I <igokulganesh@gmail.com>
2024-04-26 22:11:45 +0530

    268 Added CPP

--- C++/268. Missing Number.cpp
+++ C++/268. Missing Number.cpp
@@ -1,10 +1,14 @@
-int missingNumber(vector<int>& nums) 
+#include <vector>
+
+using namespace std;
+
+int missingNumber(vector<int> &nums)
 {
-    int n = nums.size(); 
-    int sum1 = 0, sum2 = (n*(n+1))/2 ; 
+    int n = nums.size();
+    int sum1 = 0, sum2 = (n * (n + 1)) / 2;
 
-    for(int x : nums)
-        sum1 += x ; 
+    for (int x : nums)
+        sum1 += x;
 
-    return sum2 - sum1 ;     
+    return sum2 - sum1;
 }
