5a1dd3f4d0c2a0e3ad8ce67a6e35991d85c0cefa
Gokul Ganesh I <igokulganesh@gmail.com>
2024-04-27 12:59:44 +0530

    21 Added CPP

--- C++/21. Merge Two Sorted Lists.cpp
+++ C++/21. Merge Two Sorted Lists.cpp
@@ -1,68 +1,81 @@
-// Iterative
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
 {
-    if(l1 == NULL)
-        return l2 ; 
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-    if(l2 == NULL)
-        return l1 ; 
+// Iterative
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+{
+    if (l1 == NULL)
+        return l2;
 
-    ListNode *head, *tail ; 
+    if (l2 == NULL)
+        return l1;
 
-    if(l1->val < l2->val)
+    ListNode *head, *tail;
+
+    if (l1->val < l2->val)
     {
-        head = l1 ; 
-        tail = l1 ; 
-        l1 = l1->next ; 
+        head = l1;
+        tail = l1;
+        l1 = l1->next;
     }
     else
     {
-        head = l2 ; 
-        tail = l2 ;
-        l2 = l2->next ; 
+        head = l2;
+        tail = l2;
+        l2 = l2->next;
     }
 
-    while(l1 and l2)
+    while (l1 and l2)
     {
-        if(l1->val < l2->val)
+        if (l1->val < l2->val)
         {
-            tail->next = l1 ; 
-            l1 = l1->next ; 
+            tail->next = l1;
+            l1 = l1->next;
         }
         else
         {
-            tail->next = l2 ; 
-            l2 = l2->next ; 
+            tail->next = l2;
+            l2 = l2->next;
         }
-        tail = tail->next ;
+        tail = tail->next;
     }
 
-    if(l1)
-        tail->next = l1 ; 
-    
-    if(l2)
-        tail->next = l2 ; 
+    if (l1)
+        tail->next = l1;
 
-    return head ; 
+    if (l2)
+        tail->next = l2;
+
+    return head;
 }
 
 // Recursive Approach
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
 {
-    if(l1 == NULL)
-        return l2 ; 
+    if (l1 == NULL)
+        return l2;
 
-    if(l2 == NULL)
-        return l1 ; 
+    if (l2 == NULL)
+        return l1;
 
-    if(l1->val < l2->val)
-    { 
+    if (l1->val < l2->val)
+    {
         l1->next = mergeTwoLists(l1->next, l2);
-        return l1 ;   
+        return l1;
     }
     else
-    { 
+    {
         l2->next = mergeTwoLists(l1, l2->next);
-        return l2 ; 
+        return l2;
     }
 }
