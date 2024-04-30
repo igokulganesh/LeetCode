3738e2f1cdeda51a5a7619e08545b28d1ff3c20f
Gokul Ganesh I <igokulganesh@gmail.com>
2024-04-30 22:35:54 +0530

    70 C++

--- C++/70. Climbing Stairs.cpp
+++ C++/70. Climbing Stairs.cpp
@@ -1,54 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 // Basic Solution
 int climbStairs_native(int n)
 {
-	if( n == 1 or n == 2 )
-		return n ;
-	return climbStairs_native(n-1) + climbStairs_native(n-2); 
+	if (n == 1 or n == 2)
+		return n;
+	return climbStairs_native(n - 1) + climbStairs_native(n - 2);
 }
 
 // By using Top down approach
-vector<int> dp; 
-int climbStairs_dp(int n) 
+int climbStairs(int n, vector<int> &dp)
 {
-	if(!dp[n])
-		dp[n] = climbStairs_dp(n-1) + climbStairs_dp(n-2) ;
-	
-	return dp[n] ; 
+	if (n == 1 or n == 2)
+		return n;
+
+	if (dp[n] == 0)
+		dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+
+	return dp[n];
 }
 
 int climbStairs(int n)
 {
-	dp = vector<int>(n+1, 0); 
-	dp[1] = 1 ; 
-	dp[2] = 2 ;
-	return climbStairs_dp(n) ; 
+	vector<int> dp(n + 1, 0);
+	return climbStairs(n, dp);
 }
 
 // By using bottom up approach
 int climbStairs(int n)
 {
-	dp = vector<int>(n+1, 0); 
-	dp[1] = 1 ; 
-	dp[2] = 2 ;
+	if (n == 1 or n == 2)
+		return n;
 
-	for(int i = 3 ; i <= n ; i++ )
-	{
-		dp[i] = dp[i-1] + dp[i-2] ; 
-	} 
-	return dp[n] ; 
+	vector<int> dp(n + 1, 0);
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+		dp[i] = dp[i - 1] + dp[i - 2];
+
+	return dp[n];
 }
 
 // Optimizing the Space complexity
+int climbStairs(int n)
+{
+	if (n == 1 or n == 2)
+		return n;
+
+	int prev1 = 1, prev2 = 2;
+	int cur;
+
+	for (int i = 3; i <= n; i++)
+	{
+		cur = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = cur;
+	}
+	return cur;
+}
 
 int climbStairs(int n)
 {
-	int a = 0, b = 1, c = 1 ; 
+	int a = 0, b = 1, c = 1;
 
-	for(int i = 1 ; i < n ; i++)
+	for (int i = 1; i < n; i++)
 	{
-		a = b ; 
-		b = c ; 
-		c = a + b ; 
+		a = b;
+		b = c;
+		c = a + b;
 	}
-	return c ;
+	return c;
 }
