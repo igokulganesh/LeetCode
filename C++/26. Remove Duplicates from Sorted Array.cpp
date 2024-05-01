5a9751c8abf89c40c3fb2f3657712fbe0de4b2f0
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-01 18:52:58 +0530

    C++ Sol: 26

--- C++/26. Remove Duplicates from Sorted Array.cpp
+++ C++/26. Remove Duplicates from Sorted Array.cpp
@@ -2,35 +2,34 @@
 
 using namespace std;
 
-int removeDuplicates(vector<int>& nums) 
+int removeDuplicates(vector<int> &nums)
 {
-	int dup = nums[0], replace_index = 1;
+    int dup = nums[0], replace_index = 1;
 
-	for(int i = 1; i < nums.size(); i++)
-	{
-		if(dup != nums[i])
-		{
-			dup = nums[i];
-			nums[replace_index++] = nums[i];
-		}
-	}
-	return replace_index-1; 
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (dup != nums[i])
+        {
+            dup = nums[i];
+            nums[replace_index++] = nums[i];
+        }
+    }
+    return replace_index - 1;
 }
 
-// best solution
-int removeDuplicates(vector<int>& nums) 
+int removeDuplicates(vector<int> &nums)
 {
-    if(nums.size() <= 1)
-        return nums.size() ; 
-    
-    int k = 1 ; 
-    for(int i = 1 ; i < nums.size() ; i++)
-    {
-        if(nums[i-1] == nums[i])
-            continue ; 
+    if (nums.size() <= 1)
+        return nums.size();
 
-        nums[k++] = nums[i] ; 
+    int k = 1;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i - 1] == nums[i])
+            continue;
+
+        nums[k++] = nums[i];
     }
 
-    return k ; 
+    return k;
 }
