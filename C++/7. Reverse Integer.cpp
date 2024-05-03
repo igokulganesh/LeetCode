36874cc1bc385e3ee7be5de035edc95498d8ba59
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-03 23:26:33 +0530

    C++ Sol: 7

--- C++/7. Reverse Integer.cpp
+++ C++/7. Reverse Integer.cpp
@@ -1,12 +1,34 @@
-int reverse(int x) 
-{
- 	long res = 0 ; 
+#include <bits/stdc++.h>
 
-	while(x)
+using namespace std;
+
+int reverse(int x)
+{
+	long res = 0;
+
+	while (x)
 	{
-		res = (res * 10) + (x%10) ; 
-		x /= 10 ; 
+		res = (res * 10) + (x % 10);
+		x /= 10;
 	}
 
-	return (res > INT_MAX or res < INT_MIN) ? 0 : res  ; 
+	return (res > INT_MAX or res < INT_MIN) ? 0 : res;
+}
+
+int reverse(int x)
+{
+	int res = 0;
+
+	while (x)
+	{
+		if (res > INT_MAX / 10 or res < INT_MIN / 10)
+		{
+			res = 0;
+			break;
+		}
+		res = (res * 10) + (x % 10);
+		x /= 10;
+	}
+
+	return res;
 }
