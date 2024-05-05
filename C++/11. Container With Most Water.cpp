15f9f3e07d9b37507c7b861387b9bda30ab16753
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-05 22:52:17 +0530

    C++ Sol: 11

--- C++/11. Container With Most Water.cpp
+++ C++/11. Container With Most Water.cpp
@@ -1,34 +1,38 @@
+#include <vector>
+
+using namespace std;
+
 // Brutte Force Method
-int maxArea(vector<int>& height) 
+int maxArea(vector<int> &height)
 {
-    int res = 0 ; 
-    
-    for(int i = 0 ; i < height.size() ; i++)
-    {
-        for(int j = i+1 ; j < height.size() ; j++)
-        {
-            res = max(res, min(height[i], height[j]) * (j - i)); 
-        }
-    }
-    
-    return res ;
-}	
+	int res = 0;
+
+	for (int i = 0; i < height.size(); i++)
+	{
+		for (int j = i + 1; j < height.size(); j++)
+		{
+			res = max(res, min(height[i], height[j]) * (j - i));
+		}
+	}
+
+	return res;
+}
 
 // Optimal Apporach
-int maxArea(vector<int>& height) 
+int maxArea(vector<int> &height)
 {
-	int res = 0 ; 
-	int low = 0, high = height.size()-1 ; 
+	int res = 0;
+	int low = 0, high = height.size() - 1;
 
-	while(low < high)
+	while (low < high)
 	{
-		res = max(res, (high-low) * min(height[low], height[high])) ; 
+		res = max(res, (high - low) * min(height[low], height[high]));
 
-		if(height[low] < height[high])
-			low++ ; 
+		if (height[low] < height[high])
+			low++;
 		else
-			high-- ; 
-	} 
+			high--;
+	}
 
-	return res ; 
+	return res;
 }
