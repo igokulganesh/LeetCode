16c2fb4a5ad57521c04dcd62a176fc62947c94b6
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-09 21:28:54 +0530

    C++ Sol: 844

--- C++/844. Backspace String Compare.cpp
+++ C++/844. Backspace String Compare.cpp
@@ -1,90 +1,94 @@
-bool backspaceCompare(string s, string t) 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool backspaceCompare(string s, string t)
 {
-    int i = s.size()-1 ;
-    int j = t.size()-1 ;
-    int bS = 0, bT = 0; 
-    while( i >= 0 or j >= 0 )
-    { 
-        while(i >= 0 )
-        {    
-            if(s[i] == '#')
+    int i = s.size() - 1;
+    int j = t.size() - 1;
+    int bS = 0, bT = 0;
+    while (i >= 0 or j >= 0)
+    {
+        while (i >= 0)
+        {
+            if (s[i] == '#')
             {
-                i-- ; 
-                bS++ ;
+                i--;
+                bS++;
             }
-            else if(bS > 0 )
+            else if (bS > 0)
             {
-                bS-- ;
-                i-- ; 
-            } 
+                bS--;
+                i--;
+            }
             else
-                break ; 
+                break;
         }
 
-        while(j >= 0)
+        while (j >= 0)
         {
 
-            if(t[j] == '#')
+            if (t[j] == '#')
             {
-                j-- ; 
-                bT++ ; 
+                j--;
+                bT++;
             }
-            else if(bT > 0)
+            else if (bT > 0)
             {
-                j-- ; 
-                bT-- ; 
+                j--;
+                bT--;
             }
-            else 
-                break ; 
+            else
+                break;
         }
 
-        if( i >= 0 and j >= 0 and s[i] != t[j]) 
-            return false ;
+        if (i >= 0 and j >= 0 and s[i] != t[j])
+            return false;
 
-        if((i >= 0) != (j >= 0))
-            return false ; 
+        if ((i >= 0) != (j >= 0))
+            return false;
 
-        i-- ; 
-        j-- ;  
+        i--;
+        j--;
     }
 
-    return true ; 
-}   
+    return true;
+}
 
 // Removing every # and do backspace and then compare (Not Recommended)
-bool backspaceCompare(string &s , string &t) 
+bool backspaceCompare(string &s, string &t)
 {
-    int i ; 
+    int i;
 
-    for( i = 0 ; i < s.size() ; i++)
+    for (i = 0; i < s.size(); i++)
     {
-        if( i > 0 and s[i] == '#')
+        if (i > 0 and s[i] == '#')
         {
-            s.erase(i,1) ;
-            s.erase(i-1,1) ;
-            i -= 2 ;
+            s.erase(i, 1);
+            s.erase(i - 1, 1);
+            i -= 2;
         }
-        else if(s[0] == '#')
+        else if (s[0] == '#')
         {
-            s.erase(0,1) ;
-            i-- ; 
+            s.erase(0, 1);
+            i--;
         }
     }
 
-    for( i = 0 ; i < t.size() ; i++)
+    for (i = 0; i < t.size(); i++)
     {
-        if( i > 0 and t[i] == '#')
+        if (i > 0 and t[i] == '#')
         {
-            t.erase(i,1) ;
-            t.erase(i-1,1) ;
-            i -= 2 ;
+            t.erase(i, 1);
+            t.erase(i - 1, 1);
+            i -= 2;
         }
-        else if(t[0] == '#')
+        else if (t[0] == '#')
         {
-            t.erase(0,1) ;
-            i-- ; 
+            t.erase(0, 1);
+            i--;
         }
     }
 
-    return (s == t) ;
+    return (s == t);
 }
