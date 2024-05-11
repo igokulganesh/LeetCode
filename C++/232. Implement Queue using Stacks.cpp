88a288f82c82e395b932f5fe66760fba8b149891
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-11 22:53:24 +0530

    C++ Sol: 232

--- C++/232. Implement Queue using Stacks.cpp
+++ C++/232. Implement Queue using Stacks.cpp
@@ -1,28 +1,31 @@
-class MyQueue {
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyQueue
+{
 public:
-    
-    stack<int> s1, s2 ;
+    stack<int> s1, s2;
 
     /** Initialize your data structure here. */
-    MyQueue() 
-    {}
+    MyQueue() {}
 
     /** Push element x to the back of queue. */
-    void push(int x) 
+    void push(int x)
     {
-        if(s1.empty())
+        if (s1.empty())
             s1.push(x);
         else
         {
-            while(!s1.empty())
+            while (!s1.empty())
             {
-                s2.push(s1.top()); 
-                s1.pop(); 
+                s2.push(s1.top());
+                s1.pop();
             }
 
             s1.push(x);
 
-            while(!s2.empty())
+            while (!s2.empty())
             {
                 s1.push(s2.top());
                 s2.pop();
@@ -31,32 +34,22 @@ public:
     }
 
     /** Removes the element from in front of queue and returns that element. */
-    int pop() 
+    int pop()
     {
-        int x = s1.top(); 
-        s1.pop(); 
-        return x ; 
+        int x = s1.top();
+        s1.pop();
+        return x;
     }
 
     /** Get the front element. */
-    int peek() 
+    int peek()
     {
-       return s1.top();
+        return s1.top();
     }
 
     /** Returns whether the queue is empty. */
-    bool empty() 
+    bool empty()
     {
-        return s1.empty(); 
+        return s1.empty();
     }
-  
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
