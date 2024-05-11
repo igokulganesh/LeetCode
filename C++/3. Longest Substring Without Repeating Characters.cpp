0215b0dc974530cf56c788306a51e3dd818cf426
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-11 16:48:19 +0530

    C++ Sol: 3

--- C++/3. Longest Substring Without Repeating Characters.cpp
+++ C++/3. Longest Substring Without Repeating Characters.cpp
@@ -1,116 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 /*
 	Sliding Window Technique
 	Time : O(n)
 	space : Constant
 */
-int lengthOfLongestSubstring(string s) 
+int lengthOfLongestSubstring(string s)
 {
-    vector<bool> hashTable(256, false); 
+	vector<bool> characters(256, false);
+	int longest_str = 0;
+	int start = 0;
 
-    int maxLen = 0, start = 0, n = s.size() ; 
-
-    for(int i = 0 ; i < n ; i++)
-    {
-    	while( i != start and hashTable[s[i]])
-    	{
-    		hashTable[s[start++]] = 0 ; 
-    	}
-
-    	hashTable[s[i]] = true ;
-
-    	maxLen = max(maxLen, i-start+1);  
-    }
-
-    return maxLen ; 
-}
-
-
-/*
-	Dynamic Programming Approach 
-	bottom up approach which takes n*n 
-	Timelimit exceeds 
-*/
-
-int lengthOfLongestSubstring(string s) 
-{
-	int n = s.size() ; 
-
-	if(n == 0 or n == 1)
-		return n ; 
-
-	vector<vector<bool> > dp(n, vector<bool>(n, 0)); 
-
-	int maxLen = 1 ; 
-	for(int i = 0 ; i < n ; i++)
+	for (int i = 0; i < s.size(); i++)
 	{
-		dp[i][i] = 1 ; 
+		while (i != start and characters[s[i]])
+			characters[s[start++]] = false;
+
+		characters[s[i]] = true;
+
+		longest_str = max(longest_str, i - start + 1);
 	}
 
-	for(int i = 0 ; i < n-1 ; i++)
+	return longest_str;
+}
+
+/*
+	Dynamic Programming Approach
+	bottom up approach which takes n*n
+	Timelimit exceeds
+*/
+int lengthOfLongestSubstring(string s)
+{
+	int n = s.size();
+
+	if (n == 0 or n == 1)
+		return n;
+
+	vector<vector<bool>> dp(n, vector<bool>(n, 0));
+
+	int maxLen = 1;
+	for (int i = 0; i < n; i++)
 	{
-		if(s[i] != s[i+1])
+		dp[i][i] = 1;
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (s[i] != s[i + 1])
 		{
-			dp[i][i+1] = 1 ;
-			maxLen = 2 ;   
+			dp[i][i + 1] = 1;
+			maxLen = 2;
 		}
 	}
 
-	for(int len = 3 ; len <= n ; len++)
+	for (int len = 3; len <= n; len++)
 	{
-		for(int i = 0 ; i < n-len+1 ; i++)
+		for (int i = 0; i < n - len + 1; i++)
 		{
-			int j = i + len - 1; 
+			int j = i + len - 1;
 
-			if(s[i] != s[j] and dp[i][j-1] and dp[i+1][j])
+			if (s[i] != s[j] and dp[i][j - 1] and dp[i + 1][j])
 			{
-				dp[i][j] = 1 ; 
-				maxLen = len ; 
+				dp[i][j] = 1;
+				maxLen = len;
 			}
 		}
 	}
 
-	return maxLen ; 
+	return maxLen;
 }
-
 /*
-	Here is more optimixed solution 
+  G O K U L
+G 1 1
+O   1 1
+K     1 1
+U       1 1
+L 		  1
 */
-
-int lengthOfLongestSubstring(string s) 
-{
-	int n = s.size() ; 
-
-	if(n <= 1)
-		return n ; 
-
-	int maxLen = 1 ; 
-	int curLen = 1 ; 
-	int pi = 0 ; // past index  
-
-	vector<int> Hash(256, 0) ; 
-	
-	Hash[s[0]]++ ; 
-
-	for(int i = 1 ; i < n ; i++)
-	{
-		curLen++ ; 
-
-		if(Hash[s[i]])
-		{
-			while(pi < i)
-			{
-				Hash[s[pi]] = 0 ; 
-				pi++ ;
-				curLen-- ; 
-
-				if(s[pi] != s[i] and Hash[s[i]] == 0 )
-					break ; 
-			}
-		}
-		Hash[s[i]]++ ;
-
-		maxLen = max(maxLen, curLen) ; 
-	}
-
-	return maxLen ; 
-}
