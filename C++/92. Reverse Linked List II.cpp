cc301dcd70ff7c58a4c84dea155ccf76b8f9ee01
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-11 18:30:20 +0530

    C++ Sol: 92

--- C++/92. Reverse Linked List II.cpp
+++ C++/92. Reverse Linked List II.cpp
@@ -1,45 +1,51 @@
-ListNode* find(ListNode* head, int pos)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
 {
-    ListNode* prev = head ;
-    ListNode* cur = head ; 
-    while(--pos)
-    {
-        prev = cur ;  
-        cur = cur->next ; 
-    }
-    return prev ; 
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *find(ListNode *head, int pos)
+{
+    while (--pos > 0)
+        head = head->next;
+
+    return head;
 }
 
-ListNode* reverse(ListNode *head, ListNode* end)
+ListNode *reverse(ListNode *head, ListNode *end)
 {
-    ListNode *cur = head, *prev = end->next, *next ; 
+    ListNode *cur = head, *prev = end->next, *next;
 
-    while(cur and cur != end)
+    while (cur and cur != end)
     {
-        next = cur->next ; 
-        cur->next = prev ; 
-        prev = cur ; 
-        cur = next ; 
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
     }
 
-    if(cur)
-        cur->next = prev ;
+    if (cur)
+        cur->next = prev;
 
-    return cur ; 
+    return cur;
 }
 
-ListNode* reverseBetween(ListNode* head, int left, int right) 
+ListNode *reverseBetween(ListNode *head, int left, int right)
 {
-    if(left == 1 and right == 1)
-        return head ; 
-    
-    ListNode *rNode = find(head, right);
-    ListNode *lNode = find(head, left);
-    
-    if(left == 1)
-        return reverse(head, rNode->next);
-    
+    ListNode *lNode = find(head, left - 1);          // Finding Prev of Left
+    ListNode *rNode = find(lNode, right - left + 1); // Finding right
+
+    if (left == 1)
+        return reverse(head, rNode);
+
     lNode->next = reverse(lNode->next, rNode->next);
-        
-    return head ; 
+
+    return head;
 }
