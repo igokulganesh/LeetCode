f99e8594885d91eaa6f3054b8e9216f0232c9535
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-11 17:04:18 +0530

    C++ Sol: 680

--- C++/680. Valid Palindrome II.cpp
+++ C++/680. Valid Palindrome II.cpp
@@ -1,24 +1,30 @@
-bool validPalindrome(string s) 
+#include <string>
+
+using namespace std;
+
+bool validPalindrome(string s, int i, int j)
 {
-	int i = 0, j = s.size() -1 ;
-
-	while(i < j)
+	while (i < j)
 	{
-		if(s[i] != s[j])
-			return validPalindrome(s, i+1, j) or validPalindrome(s, i, j-1); 
-		i++ ; j-- ; 
+		if (s[i] != s[j])
+			return false;
+		i++;
+		j--;
 	}
-
-	return true ; 
+	return true;
 }
 
-bool validPalindrome(string s, int i, int j) 
+bool validPalindrome(string s)
 {
-	while(i < j)
+	int i = 0, j = s.size() - 1;
+
+	while (i < j)
 	{
-		if(s[i] != s[j])
-			return false ;  
-		i++ ; j-- ; 
+		if (s[i] != s[j])
+			return validPalindrome(s, i + 1, j) or validPalindrome(s, i, j - 1);
+		i++;
+		j--;
 	}
-	return true ; 
+
+	return true;
 }
