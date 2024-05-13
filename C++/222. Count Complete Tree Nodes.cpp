8500338a4ca9177c95eb5892bfa31a88268e68d3
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-13 21:23:56 +0530

    C++ Sol: 222

--- C++/222. Count Complete Tree Nodes.cpp
+++ C++/222. Count Complete Tree Nodes.cpp
@@ -1,41 +1,56 @@
-// Iterative 
-int countNodes(TreeNode* root) 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
 {
-    int count = 0 ;
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-    stack<TreeNode*> st ; // we can also use queue 
-    
-    if(root)
-        st.push(root); 
+// Iterative
+int countNodes(TreeNode *root)
+{
+    int count = 0;
 
-    while(!st.empty())
+    stack<TreeNode *> st; // we can also use queue
+
+    if (root)
+        st.push(root);
+
+    while (!st.empty())
     {
         root = st.top();
         st.pop();
-        count++ ;
+        count++;
 
-        if(root->left)
+        if (root->left)
             st.push(root->left);
-        
-        if(root->right)
+
+        if (root->right)
             st.push(root->right);
     }
 
-    return count ;
+    return count;
 }
 
 // Recursive
 int countNodes(TreeNode *root)
 {
-    if(root == nullptr)
-        return 0 ; 
+    if (root == nullptr)
+        return 0;
 
-    return 1 + countNodes(root->left) + countNodes(root->right); 
+    return 1 + countNodes(root->left) + countNodes(root->right);
 }
 
 /*
-
 Above approach are O(n)
 we still reduce them to O(nLogn) due to complete tree
-
 */
+int countNodes(TreeNode *root)
+{
+}
