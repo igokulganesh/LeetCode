8b6769ddef41b42537e308e11bbc58e5a7a3e249
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-17 21:09:44 +0530

    C++ Sol: 98

--- C++/98. Validate Binary Search Tree.cpp
+++ C++/98. Validate Binary Search Tree.cpp
@@ -1,54 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 /*
-    Idea is simple use Inorder Traversal 
-    as we know Inorder Traversal of the BST is Sorted 
+    Idea is simple use Inorder Traversal
+    as we know Inorder Traversal of the BST is Sorted
 */
 
-
-// Iterative-Approach 
-bool isValidBST(TreeNode* root) 
+// Iterative-Approach
+bool isValidBST(TreeNode *root)
 {
-    if(root == nullptr)
-        return true ; 
-
-    stack<TreeNode*> st ; 
+    stack<TreeNode *> st;
     TreeNode *prev = nullptr;
 
-    while(root or !st.empty())
+    while (root or !st.empty())
     {
-        while(root)
+        while (root)
         {
             st.push(root);
-            root = root->left ; 
+            root = root->left;
         }
 
-        root = st.top(); 
-        st.pop(); 
+        root = st.top();
+        st.pop();
 
-        if(prev and prev->val >= root->val)
-            return false ; 
+        if (prev and prev->val >= root->val)
+            return false;
 
-        prev = root ; 
-        root = root->right ; 
-
+        prev = root;
+        root = root->right;
     }
 
-    return true ; 
+    return true;
 }
 
 // Recursive Approch
-TreeNode* prev = nullptr ;
+TreeNode *prev = nullptr;
 
-bool isValidBST(TreeNode* root)
+bool isValidBST(TreeNode *root)
 {
-    if(root == nullptr)
-        return true ;
+    if (root == nullptr)
+        return true;
 
-    if(!isValidBST(root->left))
-        return false ; 
+    if (!isValidBST(root->left))
+        return false;
 
-    if(prev and prev->val >= root->val)
-        return false ; 
+    if (prev and prev->val >= root->val)
+        return false;
 
-    prev = root ; 
-    return isValidBST(root->right); 
+    prev = root;
+    return isValidBST(root->right);
 }
