12e203eb75f29129b5300cbd18d31e80a722237e
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-17 23:01:39 +0530

    C++ Sol: 994

--- C++/994. Rotting Oranges.cpp
+++ C++/994. Rotting Oranges.cpp
@@ -1,66 +1,73 @@
-int orangesRotting(vector<vector<int>>& grid) 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int orangesRotting(vector<vector<int>> &grid)
 {
-    int m = grid.size(); 
-    int n = grid[0].size(); 
-    queue<pair<int, int>> q ; 
-    int time = 0 ; 
-    int fresh = 0 ; 
+    queue<pair<int, int>> que;
 
-    for(int i = 0 ; i < m ; i++)
+    int row = grid.size();
+    int col = grid[0].size();
+
+    int time = 0;
+    int fresh = 0;
+
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0 ; j < n ; j++)
+        for (int j = 0; j < col; j++)
         {
-            if(grid[i][j] == 2)
-                q.push({i, j}); 
-            else if(grid[i][j] == 1)
-                fresh++ ; 
+            if (grid[i][j] == 2)
+                que.push({i, j});
+
+            else if (grid[i][j] == 1)
+                fresh++;
         }
-    }    
-
-    int x, y, len ;   
-
-    while(!q.empty())
-    {
-        len = q.size();
-        while(len--)
-        {
-
-            x = q.front().first ; 
-            y = q.front().second;
-            q.pop();
-
-            if(x+1 < m and grid[x+1][y] == 1)
-            {
-                q.push({x+1, y}); 
-                grid[x+1][y] = 2 ;
-                fresh-- ; 
-            }
-
-            if(x-1 >= 0 and grid[x-1][y] == 1)
-            {
-                q.push({x-1, y}); 
-                grid[x-1][y] = 2 ;
-                fresh-- ;
-            }
-
-            if(y+1 < n and grid[x][y+1] == 1)
-            {
-                q.push({x, y+1}); 
-                grid[x][y+1] = 2 ;
-                fresh-- ; 
-            }
-
-            if(y-1 >= 0  and grid[x][y-1] == 1)
-            {
-                q.push({x, y-1}); 
-                grid[x][y-1] = 2 ;
-                fresh-- ; 
-            }
-        }  
-
-        if(!q.empty())
-            time++ ;  
     }
 
-    return (fresh == 0) ? time : -1  ; 
+    int i, j, len;
+
+    while (!que.empty())
+    {
+        len = que.size();
+        while (len--)
+        {
+
+            i = que.front().first;
+            j = que.front().second;
+            que.pop();
+
+            if (i + 1 < row and grid[i + 1][j] == 1)
+            {
+                que.push({i + 1, j});
+                grid[i + 1][j] = 2;
+                fresh--;
+            }
+
+            if (i - 1 >= 0 and grid[i - 1][j] == 1)
+            {
+                que.push({i - 1, j});
+                grid[i - 1][j] = 2;
+                fresh--;
+            }
+
+            if (j + 1 < col and grid[i][j + 1] == 1)
+            {
+                que.push({i, j + 1});
+                grid[i][j + 1] = 2;
+                fresh--;
+            }
+
+            if (j - 1 >= 0 and grid[i][j - 1] == 1)
+            {
+                que.push({i, j - 1});
+                grid[i][j - 1] = 2;
+                fresh--;
+            }
+        }
+
+        if (!que.empty())
+            time++;
+    }
+
+    return (fresh == 0) ? time : -1;
 }
