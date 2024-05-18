ad99a9eacf9a64dd76dc310d7d0785014001c903
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-18 10:18:30 +0530

    C++ Sol: 1376

--- C++/1376. Time Needed to Inform All Employees.cpp
+++ C++/1376. Time Needed to Inform All Employees.cpp
@@ -1,24 +1,32 @@
-int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int numOfMinutes(int n, int headID, vector<int> &manager, vector<int> &informTime)
 {
-    vector<vector<int>> graph(n, vector<int>()); 
-    
-    for(int i = 0 ; i < n ; i++)
-        if(manager[i] != -1)
-            graph[manager[i]].push_back(i); 
+    vector<vector<int>> graph(n, vector<int>());
 
-    int minutes = 0, len, u, v ;
-    queue<pair<int, int>> q ;
-    q.push({headID, 0});
+    for (int i = 0; i < n; i++)
+        if (manager[i] != -1)
+            graph[manager[i]].push_back(i);
 
-    while(!q.empty())
+    queue<pair<int, int>> que;
+    que.push({headID, 0});
+
+    int total_minutes = 0;
+    int id, time_taken;
+
+    while (!que.empty())
     {
-        u = q.front().first ;
-        v = q.front().second ;
-        q.pop();
-        minutes = max(minutes, v);
+        id = que.front().first;
+        time_taken = que.front().second;
+        que.pop();
 
-        for(int x : graph[u])
-            q.push({x, v + informTime[u]});
+        total_minutes = max(total_minutes, time_taken);
+
+        for (int child : graph[id])
+            que.push({child, time_taken + informTime[id]});
     }
-    return minutes ;
+
+    return total_minutes;
 }
