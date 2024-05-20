3a3f2df03feadaa589d57d0fbc19b8b8bc98e7dc
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-20 22:57:25 +0530

    C++ Sol: 746

--- C++/746. Min Cost Climbing Stairs.cpp
+++ C++/746. Min Cost Climbing Stairs.cpp
@@ -1,66 +1,66 @@
-// Basic Approach
-int minCostClimbingStairs(vector<int>& cost, int i)
-{
-    if(cost.size() <= i)
-        return 0 ; 
+#include <bits/stdc++.h>
 
-    return cost[i] + min( minCostClimbingStairs(cost, i+1), minCostClimbingStairs(cost, i+2)); 
+using namespace std;
+
+// Basic Approach
+int minCostClimbingStairs(vector<int> &cost, int i)
+{
+    if (cost.size() <= i)
+        return 0;
+
+    return cost[i] + min(minCostClimbingStairs(cost, i + 1), minCostClimbingStairs(cost, i + 2));
 }
 
-int minCostClimbingStairs(vector<int>& cost) 
+int minCostClimbingStairs(vector<int> &cost)
 {
-    return min(minCostClimbingStairs(cost, 0), minCostClimbingStairs(cost, 1)); 
+    return min(minCostClimbingStairs(cost, 0), minCostClimbingStairs(cost, 1));
 }
 
 // Top Down DP solution
-int minCostClimbingStairs(vector<int>& cost, int i  )
+int minCostClimbingStairs(vector<int> &cost, int i, vector<int> &cache)
 {
-    if(cost.size() <= i)
-        return 0 ; 
+    if (cost.size() <= i)
+        return 0;
 
-    if(dp[i] == -1)
-    {
-        dp[i] = cost[i] + min( minCostClimbingStairs(cost, i+1, dp), minCostClimbingStairs(cost, i+2, dp)); 
-    }
+    if (cache[i] == -1)
+        cache[i] = cost[i] + min(minCostClimbingStairs(cost, i + 1, cache), minCostClimbingStairs(cost, i + 2, cache));
 
-    return dp[i] ; 
+    return cache[i];
 }
 
-int minCostClimbingStairs(vector<int>& cost) 
+int minCostClimbingStairs(vector<int> &cost)
 {
-    vector<int> dp(cost.size(), -1);  
-    return min(minCostClimbingStairs(cost, 0, dp), minCostClimbingStairs(cost, 1, dp)); 
+    vector<int> cache(cost.size(), -1);
+    return min(minCostClimbingStairs(cost, 0, cache), minCostClimbingStairs(cost, 1, cache));
 }
 
 // Bottom Up DP Solution
-int minCostClimbingStairs(vector<int>& cost) 
+int minCostClimbingStairs(vector<int> &cost)
 {
-    int n = cost.size(); 
-    vector<int> dp(n);  
-    
-    dp[0] = cost[0] ;
-    dp[1] = cost[1] ;
+    int n = cost.size();
 
-    for(int i = 2 ; i < n ; i++)
-    {
-        dp[i] = cost[i] + min(dp[i-1], dp[i-2]); 
-    }
+    vector<int> cache(n);
+    cache[0] = cost[0];
+    cache[1] = cost[1];
 
-    return min(dp[n-1], dp[n-2]); 
+    for (int i = 2; i < n; i++)
+        cache[i] = cost[i] + min(cache[i - 1], cache[i - 2]);
+
+    return min(cache[n - 1], cache[n - 2]);
 }
 
 // Bottom Up Solution with (Space Optimized)
-int minCostClimbingStairs(vector<int> & cost)
+int minCostClimbingStairs(vector<int> &cost)
 {
-    int a = cost[0] ;
-    int b = cost[1] ; 
-    int c ; 
+    int a = cost[0];
+    int b = cost[1];
+    int c;
 
-    for(int i = 2 ; i < n ; i++)
+    for (int i = 2; i < cost.size(); i++)
     {
-        c = cost[i] + min(a, b); 
-        a = b ; 
-        b = c ;  
+        c = cost[i] + min(a, b);
+        a = b;
+        b = c;
     }
-    return min(a, b); 
+    return min(a, b);
 }
