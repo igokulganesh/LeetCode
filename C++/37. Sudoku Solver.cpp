e33c3cc5ad7bd0c7ded51fa629e3b9fe30fec40b
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-21 23:01:41 +0530

    C++ Sol: 37

--- C++/37. Sudoku Solver.cpp
+++ C++/37. Sudoku Solver.cpp
@@ -1,35 +1,40 @@
-bool isValidMove(vector<vector<char>>& b, int row, int col, char k)
+#include <vector>
+
+using namespace std;
+
+bool isValidMove(vector<vector<char>> &board, int row, int col, char move)
 {
     int b_row = (row / 3) * 3, b_col = (col / 3) * 3;
-    for(int i = 0 ; i < 9 ; i++)
+    for (int i = 0; i < 9; i++)
     {
-        if(b[row][i] == k or b[i][col] == k or b[b_row + i / 3][b_col + i % 3] == k)
-            return false ; 
+        if (board[row][i] == move or board[i][col] == move or board[b_row + i / 3][b_col + i % 3] == move)
+            return false;
     }
-    return true ; 
+
+    return true;
 }
 
-bool solveSudoku(vector<vector<char>>& board) 
+bool solveSudoku(vector<vector<char>> &board)
 {
-    for(int i = 0 ; i < 9 ; i++)
+    for (int i = 0; i < 9; i++)
     {
-        for(int j = 0 ; j < 9 ; j++)
+        for (int j = 0; j < 9; j++)
         {
-            if(board[i][j] != '.')
-                continue ; 
+            if (board[i][j] != '.')
+                continue;
 
-            for(char k = '1' ; k <= '9' ; k++ ) 
+            for (char move = '1'; move <= '9'; move++)
             {
-                if(isValidMove(board, i, j, k))
+                if (isValidMove(board, i, j, move))
                 {
-                    board[i][j] = k ; 
-                    if(solveSudoku(board))
-                        return true ;
-                    board[i][j] = '.' ; 
+                    board[i][j] = move;
+                    if (solveSudoku(board))
+                        return true;
+                    board[i][j] = '.';
                 }
             }
-            return false ; 
+            return false;
         }
     }
-    return true ; 
+    return true;
 }
