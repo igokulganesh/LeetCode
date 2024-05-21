65eb841dfe4bb42013c4ee4731bf1fd2cdd68efa
Gokul Ganesh I <igokulganesh@gmail.com>
2024-05-21 23:09:31 +0530

    C++ Sol: 131

--- C++/131. Palindrome Partitioning.cpp
+++ C++/131. Palindrome Partitioning.cpp
@@ -1,42 +1,48 @@
-bool isPalindrome(string & s)
-{
-    int l = 0, h = s.size() -1 ; 
+#include <bits/stdc++.h>
 
-    while(l < h)
+using namespace std;
+
+bool isPalindrome(string &s)
+{
+    int left = 0, right = s.size() - 1;
+
+    while (left < right)
     {
-        if(s[l] != s[h])
-            return false ; 
-        l++ , h-- ; 
+        if (s[left] != s[right])
+            return false;
+        left++, right--;
     }
-    return true ; 
+    return true;
 }
 
-void partition(string s, vector<string> & cur, vector<vector<string>> & res)
+void partition(string s, vector<string> &cur, vector<vector<string>> &res)
 {
-    if(s.empty())
+    if (s.empty())
     {
-        res.push_back(cur); 
-        return ; 
+        res.push_back(cur);
+        return;
     }
 
-    for(int i = 1 ; i <= s.size() ; i++)
+    for (int i = 1; i <= s.size(); i++)
     {
-        string left(s.begin(), s.begin()+i); 
-        string right(s.begin()+i, s.end());
+        string left(s.begin(), s.begin() + i);
 
-        if(isPalindrome(left))
+        if (isPalindrome(left))
         {
             cur.push_back(left);
-            partition(right, cur, res); 
+
+            string right(s.begin() + i, s.end());
+            partition(right, cur, res);
+
             cur.pop_back(); // Backtracking
         }
     }
 }
 
-vector<vector<string>> partition(string s) 
+vector<vector<string>> partition(string s)
 {
-    vector<vector<string>> res ;
-    vector<string> cur ; 
-    partition(s, cur, res); 
-    return res ; 
+    vector<vector<string>> res;
+    vector<string> cur;
+    partition(s, cur, res);
+    return res;
 }
