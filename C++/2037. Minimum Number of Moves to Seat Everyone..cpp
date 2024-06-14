b6b1e0eb1d05e19b170245d16b356d58c10da93b
Gokul Ganesh I <igokulganesh@gmail.com>
2024-06-14 20:16:30 +0530

    C++ Sol: 2037

---
+++ C++/2037. Minimum Number of Moves to Seat Everyone..cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minMovesToSeat(vector<int> &seats, vector<int> &students)
+{
+    sort(seats.begin(), seats.end());
+    sort(students.begin(), students.end());
+
+    int min_moves = 0;
+
+    for (int i = 0; i < seats.size(); i++)
+        min_moves += abs(seats[i] - students[i]);
+
+    return min_moves;
+}
