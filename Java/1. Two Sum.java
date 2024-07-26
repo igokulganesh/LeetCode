12c3609b2efdf76160dbbf2c87a578254f5c8304
Gokul Ganesh I <igokulganesh@gmail.com>
2024-07-26 21:45:12 +0530

    Java Sol: 1

---
+++ Java/1. Two Sum.java
@@ -0,0 +1,29 @@
+// Brute-Force Solution
+
+import java.util.HashMap;
+
+class BruteForceSolution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+        return null;
+    }
+}
+
+class HashMapSolution {
+    public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && !map.get(target - nums[i]).equals(i)) {
+                return new int[] { i, map.get(target - nums[i]) };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    };
+}
