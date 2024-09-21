ddee6999b829d017dcfc26ffe7e04bd27886f980
Gokul Ganesh I <igokulganesh@gmail.com>
2024-09-21 17:36:03 +0530

    Java Sol: 1

--- Java/1. Two Sum.java
+++ Java/1. Two Sum.java
@@ -17,11 +17,11 @@ class Solution {
         var map = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && !map.get(target - nums[i]).equals(i)) {
+            if (map.containsKey(target - nums[i])) {
                 return new int[] { i, map.get(target - nums[i]) };
             }
             map.put(nums[i], i);
         }
         return null;
-    };
+    }
 }
