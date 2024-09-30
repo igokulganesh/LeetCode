7c54c284de1dcb8a2f0900767faedc1daf9df09d
Gokul Ganesh I <igokulganesh@gmail.com>
2024-09-30 19:05:05 +0530

    Java Sol: 100

---
+++ Java/100. Same Tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null || p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    /* ignore-boiler-code */
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
