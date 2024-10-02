d105fc873a9e47e02321bb1aae327f62ed5b3464
Gokul Ganesh I <igokulganesh@gmail.com>
2024-10-02 12:36:39 +0530

    Java Sol: 222

---
+++ Java/222. Count Complete Tree Nodes.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    /* ignore-boiler-code */
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
