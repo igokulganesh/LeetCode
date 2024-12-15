dc980a81d5615829580b68146f822b516d5518ed
Gokul Ganesh I <igokulganesh@gmail.com>
2024-12-15 11:21:12 +0530

    Java Sol: 152

---
+++ Java/152. Maximum Product Subarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = 0, prod = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            prod *= nums[i];
+            max = Math.max(max, prod);
+
+            if (prod == 0)
+                prod = 1;
+        }
+
+        prod = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            prod *= nums[i];
+            max = Math.max(max, prod);
+
+            if (prod == 0)
+                prod = 1;
+        }
+
+        return max;
+    }
+}
